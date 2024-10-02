91cfd7b153b3e5db57cf17c7fe4e42461b38a806
Isabel María Cruz Lorite <166284628+IsaLorito@users.noreply.github.com>
2024-10-02 20:56:22 +0200

    Update index.js

--- docs/src/components/HomepageFeatures/index.js
+++ docs/src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Formación académica',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('@site/static/img/formacion.svg').default,
     description: (
       <>
         Docusaurus was designed from the ground up to be easily installed and
@@ -15,7 +15,7 @@ const FeatureList = [
   },
   {
     title: 'Experiencia investigadora',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    Svg: require('@site/static/img/investigacion.svg').default,
     description: (
       <>
         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
@@ -25,7 +25,7 @@ const FeatureList = [
   },
   {
     title: 'Experiencia docente',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Svg: require('@site/static/img/docencia.svg').default,
     description: (
       <>
         Extend or customize your website layout by reusing React. Docusaurus can
